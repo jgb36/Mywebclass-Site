fb51d8f0b74928eeb81e6dbcbaf30deb61078122
Jason-Bilawsky <jgb36@njit.edu>
2023-04-27 12:58:14 -0400

    template added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '../styles/global.css'
+import "../styles/tailwind.css";
+import "../styles/slick.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
